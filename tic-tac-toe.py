c5faa948058e003f2559aa9f56f6e9516e412fa6
Taeyoung Yoon <yunty1017@gmail.com>
2017-11-26 20:24:05 +0900

    add function

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -12,9 +12,38 @@ def checked(i) :
       if player == "X" :
             player = "O"
             button["bg"] = "yellow"
+            winline()
       else :
             player = "X"
             button["bg"] = "lightgreen"
+            winline()
+
+def winline():
+      if list[0]["text"]==list[1]["text"]==list[2]["text"] != "     ":
+            winner()
+      elif list[0]["text"]==list[3]["text"]==list[6]["text"] != "     ":
+            winner()
+      elif list[0]["text"]==list[4]["text"]==list[8]["text"] != "     ":
+            winner()
+      elif list[1]["text"]==list[4]["text"]==list[7]["text"] != "     ":
+            winner()
+      elif list[2]["text"]==list[4]["text"]==list[6]["text"] != "     ":
+            winner()
+      elif list[2]["text"]==list[5]["text"]==list[8]["text"] != "     ":
+            winner()
+      elif list[3]["text"]==list[4]["text"]==list[5]["text"] != "     ":
+            winner()
+      elif list[6]["text"]==list[7]["text"]==list[8]["text"] != "     ":
+            winner()
+
+def winner():
+      if player == "X" :
+            msg = Message(window, text="O player winner")
+            msg.grid(row=3, column=0)
+            
+      else :
+            msg = Message(window, text="X player winner")
+            msg.grid(row=3, column=0)
 
 window = Tk()
 player = "X"
